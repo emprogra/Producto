264361ae9f8c6474dd19c58ed5ceeb01f8eb0d31
Emily <emi.roca18@gmail.com>
2021-02-26 17:32:40 -0400

    contador de la vocal a y contador de consonantes (prueba exitosa en Postman)

--- Primera practica/Producto1/Controllers/ProductoController.cs
+++ Primera practica/Producto1/Controllers/ProductoController.cs
@@ -5,17 +5,47 @@
     [Route("api/[controller]")]
     [ApiController]
     public class ProductoController : ControllerBase
-    {   
+    {
         [HttpGet]
-        public int Get([FromHeader]string palabra)
+        public int Get([FromHeader] string palabra)
         {
-            int contador=0;
-            for (int i = 0; i <= palabra.Length-1; i++) 
+            int contador = 0;
+            for (int i = 0; i <= palabra.Length - 1; i++)
             {
-                if ((palabra[i].ToString().ToLower()=="a")||(palabra[i].ToString().ToLower()=="e")||(palabra[i].ToString().ToLower()=="i")||(palabra[i].ToString().ToLower()=="o")||(palabra[i].ToString().ToLower()=="u"))
+                if ((palabra[i].ToString().ToLower() == "a") || (palabra[i].ToString().ToLower() == "e") || (palabra[i].ToString().ToLower() == "i") || (palabra[i].ToString().ToLower() == "o") || (palabra[i].ToString().ToLower() == "u"))
                 {
-                    contador++;         
+                    contador++;
 
+                }
+            }
+            return contador;
+
+        }
+        [HttpGet]
+        [Route("GetA")]
+        public int GetA([FromHeader] string palabra)
+        {
+            int contador = 0;
+            for (int i = 0; i <= palabra.Length - 1; i++)
+            {
+                if ((palabra[i].ToString().ToUpper() == "A"))
+                {
+                    contador++;
+                }
+
+            }
+            return contador;
+        }
+        [HttpGet]
+        [Route("GetB")]
+        public int GetB([FromHeader] string palabra)
+        {
+            int contador = 0;
+            for(int i=0;i<=palabra.Length-1;i++)
+            {
+                if((palabra[i].ToString().ToLower()!="a")&&(palabra[i].ToString().ToLower()!="e")&&(palabra[i].ToString().ToLower()!="i")&&(palabra[i].ToString().ToLower()!="o")&&(palabra[i].ToString().ToLower()!="u"))
+                {
+                    contador++;
                 }
             }
             return contador;
